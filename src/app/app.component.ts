09b3da339cd0477da3b9e751eb30fc8e0d7bb7ed
Olexander Husak <valavanca@gmail.com>
2017-06-26 19:24:08 +0300

    initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input }   from '@angular/core';
+
+import { FormDataService }   from './data/formData.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'form-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    title = 'Multi-Step Wizard';
+    @Input() formData;
+    
+    constructor(private formDataService: FormDataService) {
+    }
+ 
+    ngOnInit() {
+        this.formData = this.formDataService.getFormData();
+        console.log(this.title + ' loaded!');
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }        from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+/* Material */
+import { MaterialModule } from '@angular/material';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
+/* App root*/
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+/* Fields components*/
+import { MoneyReturnComponent } from './money-return/money-return.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { ConfirmComponent } from './confirm/confirm.component';
+
+/* App Router */
+import { AppRoutingModule } from './app-routing.module';
+
+/* Shared Service */
+import { FormDataService }    from './data/formData.service'
+import { WorkflowService }    from './workflow/workflow.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MoneyReturnComponent,
+    PersonalInfoComponent,
+    ConfirmComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    NoopAnimationsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: FormDataService, useClass: FormDataService },
+                    { provide: WorkflowService, useClass: WorkflowService }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
